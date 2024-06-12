4419ba2325812a4a414e25ac6ce7ec5d79bd2c3e
Ventior <SamuelBeck16@gmail.com>
2024-06-12 23:12:16 +0200

    fixed javascript for theme setter and css for theme button

--- public/js/theme-toggle.js
+++ public/js/theme-toggle.js
@@ -1,3 +1,27 @@
+function setTheme() {
+    let currentTheme = localStorage.getItem('theme');
+
+    if (!currentTheme) {
+        // If savedTheme is empty, set it to 'light'
+        currentTheme = 'light-theme';
+    }
+
+
+    // Save the current theme to localStorage
+    if (currentTheme === 'dark') {
+        document.body.classList.add('dark-theme');
+        document.body.classList.remove('light-theme');
+    } else {
+        document.body.classList.add('light-theme');
+        document.body.classList.remove('dark-theme');
+    }
+}
+debugger
+setTheme();
+
+
+
+
 const nav = document.querySelector('.mobile-nav');
 const navMenuBtn = document.querySelector('.nav-menu-btn');
 const navCloseBtn = document.querySelector('.nav-close-btn');
@@ -19,16 +43,16 @@ for (let i = 0; i < themeBtn.length; i++) {
 
     themeBtn[i].addEventListener('click', function toggle_theme() {
 
-        // toggle `light-theme` & `dark-theme` class from `body`
-        // when clicked `theme-btn`
+
+
         document.body.classList.toggle('light-theme');
         document.body.classList.toggle('dark-theme');
-
-        // Determine the current theme
+        //
+        // // Determine the current theme
         const currentTheme = document.body.classList.contains('dark-theme')? 'dark' : 'light';
-
-        // Save the current theme to localStorage
-        if (currentTheme!== 'dark') {
+        //
+        // // Save the current theme to localStorage
+        if (currentTheme === 'dark') {
             localStorage.setItem('theme', 'dark');
         } else {
             localStorage.setItem('theme', 'light');
@@ -38,20 +62,12 @@ for (let i = 0; i < themeBtn.length; i++) {
 
             // When the `theme-btn` is clicked,
             // it toggles classes between `light` & `dark` for all `theme-btn`.
-            themeBtn[i].classList.toggle('light');
             themeBtn[i].classList.toggle('dark');
-
+            themeBtn[i].classList.toggle('light');
+            // themeBtn[i].classList.toggle('active');
         }
 
     })
 
 }
 
-// function toggleTheme() {
-//     if (localStorage.getItem('theme') != 'dark') {
-//         localStorage.setItem('theme', 'dark');
-//     } else {
-//         localStorage.setItem('theme', 'light');
-//     }
-//     setTheme();
-// }

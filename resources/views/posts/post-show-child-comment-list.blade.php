872ea0108021f8561f25cc78b30137bbb1f6763a
Ventior <SamuelBeck16@gmail.com>
2024-06-16 02:42:33 +0200

    changed indent level on commentarea livewire component

--- resources/views/posts/post-show-child-comment-list.blade.php
+++ resources/views/posts/post-show-child-comment-list.blade.php
@@ -1,7 +1,7 @@
 @if(count((array)$comments))
     @foreach($comments as $reply)
         <x-comment :post="$post" :comment="$reply" :indent="$indent_level"/>
-        <livewire:commentarea :post="$post" :comment="$reply" :indent="0" />
+        <livewire:commentarea :post="$post" :comment="$reply" :indent="$indent_level" />
 {{--        <x-comment-box :post="$post" :comment="$reply" :indent="$indent_level"/>--}}
         @if($reply->replies->count()>0)
             @include('posts.post-show-child-comment-list',['comments'=>$reply->replies, 'indent_level'=>$indent_level + 1])

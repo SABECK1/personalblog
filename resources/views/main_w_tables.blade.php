e64b6015ca97309c6d0908f6734e651d80427d19
Ventior <SamuelBeck16@gmail.com>
2024-06-17 19:55:11 +0200

    better tag styling

--- resources/views/main_w_tables.blade.php
+++ resources/views/main_w_tables.blade.php
@@ -2,33 +2,39 @@
 
 
 @section('categories')
-<section name="categories" class="table-wrapper">
-    <table class="table">
-        <span class="h2">Categories</span>
-        <thead>
+    <section name="categories" class="table-wrapper">
+        <table class="table">
+            <span class="h2">Categories</span>
+            <thead>
             <tr>
                 <th></th>
                 <th>Category</th>
                 <th class="table-count">Posts</th>
             </tr>
-        </thead>
-        <tbody>
+            </thead>
+            <tbody>
             @foreach ($categories as $category)
-            <tr><td class='icon-column'><i class='{{ $category->icon }}'></i></td><td>{{ $category->category_name }}</td><td class='table-count'>{{ $category->category_count }}</td></tr>
+                <tr>
+                    <td class='icon-column'><i class='{{ $category->icon }}'></i></td>
+                    <td>{{ $category->category_name }}</td>
+                    <td class='table-count'>{{ $category->category_count }}</td>
+                </tr>
             @endforeach
-        </tbody>
-    </table>
-</section>
+            </tbody>
+        </table>
+    </section>
 @endsection
 
 @section('tags')
-<section name="tags" class="table-wrapper">
+    <section name="tags" class="table-wrapper">
 
         <span class="h2">Tags</span>
+        <div class="flex-wrapper">
             @foreach ($tags as $tag)
-                    <div class="text-wrapper-sm">
+                <div class="text-wrapper-sm text-sm">
                     {{ $tag->tag_name }}
-                    </div>
+                </div>
             @endforeach
-</section>
+        </div>
+    </section>
 @endsection

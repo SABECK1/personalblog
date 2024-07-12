02881348cd0dcb2469498779b1993a4043dfde9c
Ventior <SamuelBeck16@gmail.com>
2024-07-12 22:58:04 +0200

    added new components

---
+++ resources/views/livewire/header.blade.php
@@ -0,0 +1,84 @@
+<div>
+<livewire:htmlhead/>
+<div class="container">
+    <div class="header">
+        <div class="desktop-nav">
+            <div class="flex-wrapper navbar">
+                <a href="{{ route('home') }}" class="">Home</a>
+                <a href="{{ route('posts') }}" class="">Posts</a>
+                <a href="{{ route('about') }}" class="">About</a>
+                <a href="{{ route('contact') }}" class="">Contact</a>
+            </div>
+
+            <div class='flex-wrapper navbar'>
+                <div id='theme-toggle' class="theme-btn">
+                    <div class="logo-dark">
+                        <i name="moon" class="fa-solid fa-moon"></i>
+                    </div>
+                    <div class="logo-light">
+                        <i name="sunny" class="fa-solid fa-sun"></i>
+                    </div>
+                </div>
+                <i class="fa-solid fa-user"></i>
+                @guest
+                    <a href="{{ route('login') }}">Login</a>
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}">Register</a>
+                    @endif
+                @else
+                    <a href="{{route('dashboard')}}">{{ Auth::user()->name }}</a>
+                    <form method="post" action="{{ route('logout') }}">
+                        @csrf
+                        <button class="btn btn-secondary" id='logout-btn'><span>Sign Out&nbsp;</span><i
+                                class="fa fa-sign-out"></i></button>
+                    </form>
+                @endguest
+            </div>
+        </div>
+
+        <div class="btn-group">
+
+            <button id="theme-toggle-mobile" class="theme-btn">
+                <div class="logo-dark">
+                    <i name="moon" class="fa-solid fa-moon"></i>
+                </div>
+                <div class="logo-light">
+                    <i name="sunny" class="fa-solid fa-sun"></i>
+                </div>
+            </button>
+
+            <button class="nav-menu-btn">
+                <i class="fa-solid fa-bars"></i>
+            </button>
+        </div>
+
+
+    </div>
+</div>
+
+<div class="mobile-nav">
+
+    <button class="nav-close-btn">
+        <i class="fa-solid fa-x"></i>
+    </button>
+
+    <div class="wrapper">
+
+        <p class="h3 nav-title">Main Menu</p>
+
+        <ul>
+            <li class="nav-item">
+                <a href="#" class="nav-link">Home</a>
+            </li>
+
+            <li class="nav-item">
+                <a href="#" class="nav-link">About Me</a>
+            </li>
+
+            <li class="nav-item">
+                <a href="#" class="nav-link">Contact</a>
+            </li>
+        </ul>
+    </div>
+</div>
+</div>
---
+++ resources/views/livewire/htmlhead.blade.php
@@ -0,0 +1,28 @@
+<div>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        {{-- FA-Icons --}}
+        <script src="https://kit.fontawesome.com/4ab20c1496.js" crossorigin="anonymous"></script>
+
+        {{-- DevICON --}}
+        <link rel="stylesheet" type='text/css' href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"/>
+
+        {{-- Vanilla CSS Stylesheet --}}
+        <link rel="stylesheet" href="{{ asset('style.css') }}">
+
+        {{-- Google Fonts --}}
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;900&display=swap" rel="stylesheet">
+
+
+        <script src="https://unpkg.com/just-validate@latest/dist/just-validate.production.min.js" defer></script>
+
+
+        {{-- Other Javascript Functionality --}}
+        <script src="{{ asset('/js/theme-toggle.js') }}" defer></script>
+    </head>
+</div>

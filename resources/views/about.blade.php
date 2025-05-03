ae8115eb4daf7c22d3059f2a192842def28f711d
SABECK1 <SamuelBeck16@gmail.com>
2025-05-04 00:30:56 +0200

    Redesigned and moved hero section to "about"

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,1 +1,34 @@
 @extends('layouts.template')
+@section('content')
+<section class="about" id="about">
+
+    <div class="container">
+
+        <div class="img-box">
+            <img src="{{ asset('images/hero.png') }}" alt="Samuel Beck" class="hero_img">
+        </div>
+
+        <div class="content">
+
+            <h2>My name is elora.</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Recusandae aut adipisci necessitatibus veniam, provident
+                vero architecto praesentium, non sequi libero corrupti molestias.
+                Architecto voluptate eligendi maiores, error magni accusamus officia.</p>
+
+            <div class="about-info">
+
+                <div class="info">
+                    <h4>Age: <span>21</span></h4>
+                    <h4>Languages: <span>German, English</span></h4>
+                    <h4>Specialisation: <span>Front-End developer</span></h4>
+                    <a href="#" class="btn btn-primary">Download CV<i class="fas fa-download"></i></a>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+</section>
+@endsection
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -2,23 +2,17 @@
 
 
 @section('content')
-    <div class="hero">
-        <div class="container">
-            <div class="hero_left">
-                <div class="h1">
-                    Hi, I'm <b><a href="#" class="accent_link">Samuel Beck</a></b>,
-                </div>
-                Junior ABAP-Developer, Python and PHP enthusiast
-                and the self-proclaimed author of <b>this blog!</b>
-            </div>
-
-{{--            <div class="hero_right">--}}
-{{--                <div class="img-box">--}}
-{{--                    <img src="{{ asset('images/hero.png') }}" alt="Samuel Beck" class="hero_img">--}}
+{{--    <div class="hero">--}}
+{{--        <div class="container">--}}
+{{--            <div class="hero_left">--}}
+{{--                <div class="h1">--}}
+{{--                    Hi, I'm <b><a href="#" class="accent_link">Samuel Beck</a></b>,--}}
 {{--                </div>--}}
+{{--                Junior ABAP-Developer, Python and PHP enthusiast--}}
+{{--                and the self-proclaimed author of <b>this blog!</b>--}}
 {{--            </div>--}}
-        </div>
-    </div>
+{{--        </div>--}}
+{{--    </div>--}}
 
     <div class="main">
         <div class="blog">
